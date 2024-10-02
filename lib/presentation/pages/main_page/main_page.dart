b799dd87f1d2a9f8850627e27a34b457fa1d1a15
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-10-02 21:00:49 +0700

    Add Navbar and NavbarItem on MainPage

--- lib/presentation/pages/main_page/main_page.dart
+++ lib/presentation/pages/main_page/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:cinetix_id/presentation/extensions/build_context_extension.dart';
+import 'package:cinetix_id/presentation/widgets/bottom_navbar.dart';
+import 'package:cinetix_id/presentation/widgets/bottom_navbar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,6 +15,8 @@ class MainPage extends ConsumerStatefulWidget {
 }
 
 class _MainPageState extends ConsumerState<MainPage> {
+  PageController pageController = PageController();
+  int selectedPage = 0;
   @override
   Widget build(BuildContext context) {
     ref.listen(
@@ -26,24 +30,54 @@ class _MainPageState extends ConsumerState<MainPage> {
       },
     );
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('CineTix ID'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text(ref.watch(userDataProvider).when(
-                  data: (data) => data.toString(),
-                  error: (error, stackTrace) => '',
-                  loading: () => 'Loading',
-                )),
-            ElevatedButton(
-                onPressed: () {
-                  ref.read(userDataProvider.notifier).logout();
-                },
-                child: const Text('Logout'))
-          ],
-        ),
+      body: Stack(
+        children: [
+          PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                selectedPage = index;
+              });
+            },
+            children: const [
+              Center(child: Text('Home')),
+              Center(child: Text('Ticket')),
+              Center(child: Text('Profile')),
+            ],
+          ),
+          BottomNavbar(
+              items: [
+                BottomNavbarItem(
+                  index: 0,
+                  isSelected: selectedPage == 0,
+                  title: 'Home',
+                  image: 'assets/images/movie.png',
+                  selectedImage: 'assets/images/movie-selected.png',
+                ),
+                BottomNavbarItem(
+                  index: 1,
+                  isSelected: selectedPage == 1,
+                  title: 'Ticket',
+                  image: 'assets/images/ticket.png',
+                  selectedImage: 'assets/images/ticket-selected.png',
+                ),
+                BottomNavbarItem(
+                  index: 2,
+                  isSelected: selectedPage == 2,
+                  title: 'Profile',
+                  image: 'assets/images/profile.png',
+                  selectedImage: 'assets/images/profile-selected.png',
+                ),
+              ],
+              onTap: (index) {
+                selectedPage = index;
+
+                pageController.animateToPage(selectedPage,
+                    duration: const Duration(milliseconds: 200),
+                    curve: Curves.easeInOut);
+              },
+              selectedIndex: 0),
+        ],
       ),
     );
   }
