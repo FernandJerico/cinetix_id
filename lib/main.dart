6a39b1d15698955d87bf72c601b07b10b5828b34
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-10-02 12:05:11 +0700

    Refactor main.dart and login_page.dart: Update theme and add Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'presentation/misc/constant.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,6 +23,17 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
+      theme: ThemeData.from(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: AppColors.saffron,
+              surface: AppColors.backgroundColor,
+              brightness: Brightness.dark),
+          useMaterial3: true,
+          textTheme: TextTheme(
+            bodyMedium: GoogleFonts.poppins(color: AppColors.whiteColor),
+            bodyLarge: GoogleFonts.poppins(color: AppColors.whiteColor),
+            labelLarge: GoogleFonts.poppins(color: AppColors.whiteColor),
+          )),
       debugShowCheckedModeBanner: false,
       routeInformationParser: ref.watch(routerProvider).routeInformationParser,
       routeInformationProvider:
---
+++ lib/presentation/misc/constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFF1F1D2B);
+  static const Color whiteColor = Color(0xFFF6F8FF);
+  static const Color saffron = Color(0xFFF4C95D);
+}
