87445ef814919f6aa17ce91609664bfad0f2f56d
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-10-07 12:01:35 +0700

    Refactor ticket page: Add ticket list and ticket widget

--- lib/presentation/pages/ticket/ticket_page.dart
+++ lib/presentation/pages/ticket/ticket_page.dart
@@ -1,10 +1,39 @@
+import 'package:cinetix_id/presentation/widgets/ticket.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class TicketPage extends StatelessWidget {
+import '../../providers/transaction_data/transaction_data_provider.dart';
+
+class TicketPage extends ConsumerWidget {
   const TicketPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListView(
+      padding: const EdgeInsets.fromLTRB(24, 80, 24, 24),
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: ref.watch(transactionDataProvider).when(
+                data: (transactions) => (transactions
+                        .where((e) =>
+                            e.title != 'Top Up' &&
+                            e.watchingTime! >=
+                                DateTime.now().millisecondsSinceEpoch)
+                        .toList()
+                      ..sort(
+                        (a, b) => a.watchingTime!.compareTo(b.watchingTime!),
+                      ))
+                    .map((transaction) => Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: Ticket(transaction: transaction),
+                        ))
+                    .toList(),
+                error: (error, stackTrace) => [],
+                loading: () => const [CircularProgressIndicator()],
+              ),
+        )
+      ],
+    );
   }
 }
